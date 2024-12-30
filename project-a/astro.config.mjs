eb97793cdaa0195738e35bf5d8a8ab26520c261b
unknown <danielechavarria9@hotmail.com>
2024-12-30 17:52:16 -0500

    funcionara?

--- project-a/astro.config.mjs
+++ project-a/astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [svelte(), tailwind()]
 });
