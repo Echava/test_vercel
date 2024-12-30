200a394d342704a8718d990dc861821bd9ceeced
unknown <danielechavarria9@hotmail.com>
2024-12-30 15:45:54 -0500

    Configurar para despliegue en Vercel

--- project-a/astro.config.mjs
+++ project-a/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte"; // Importa la integración de Svelte
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],  // Agrega la integración de Svelte
   output: 'server', 
-  adapter: node()
+  adapter: vercel()
 });
