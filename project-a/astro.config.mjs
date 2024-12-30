b56ca0a4fbf70937a6cffdf830134c3736bfb784
unknown <danielechavarria9@hotmail.com>
2024-12-30 17:01:01 -0500

    configuracion rutas

--- project-a/astro.config.mjs
+++ project-a/astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte"; // Importa la integración de Svelte
 import vercel from '@astrojs/vercel/serverless';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],  // Agrega la integración de Svelte
-  output: 'server', 
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+    webAnalytics: {
+      enabled: true,
+    },
+    devCommand: 'astro dev',
+    buildCommand: 'astro build',
+  }),
+  integrations: [svelte(), tailwind()],
 });
