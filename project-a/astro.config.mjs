6809cb0f1d7c697e6725e2906e3fb71a3c037031
unknown <danielechavarria9@hotmail.com>
2024-12-29 22:28:58 -0500

    Forzar nuevo despliegue en Vercel

--- project-a/astro.config.mjs
+++ project-a/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte"; // Importa la integración de Svelte
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],  // Agrega la integración de Svelte
-  output: 'server' 
+  output: 'server', 
+  adapter: node()
 });
